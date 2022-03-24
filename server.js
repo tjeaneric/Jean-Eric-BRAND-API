7d73aa7fef73301552e3bb2375a1a44a2c5b9c79
tjeaneric <ericjohn415@gmail.com>
2022-03-24 21:45:03 +0200

    dotenv issues

--- server.js
+++ server.js
@@ -24,9 +24,9 @@ if (process.env.NODE_ENV === 'development') {
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
+    //useCreateIndex: true,
+    //useFindAndModify: false,
+    // useUnifiedTopology: true,
   })
   .then(() => console.log('DB connected successfully!'));
 
