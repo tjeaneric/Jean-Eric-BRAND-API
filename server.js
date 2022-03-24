e6aae287f878b24ccaee4a7995005888c26c11ec
tjeaneric <ericjohn415@gmail.com>
2022-03-24 21:53:11 +0200

    dotenv issues(3)

--- server.js
+++ server.js
@@ -16,26 +16,29 @@ console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   DB = process.env.DB_DEV;
 } else if (process.env.NODE_ENV === 'test') {
-  DB = process.env.DB_TEST;
-  // mongoose
-  //   .connect(DB, {
-  //     useNewUrlParser: true,
-  //     useCreateIndex: true,
-  //     useFindAndModify: false,
-  //     useUnifiedTopology: true,
-  //   })
-  //   .then(() => console.log('DB connected successfully!'));
+  // DB = process.env.DB_TEST;
+  mongoose
+    .connect(DB, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log('DB connected successfully!'));
 } else if (process.env.NODE_ENV === 'production') {
   DB = process.env.DB_PROD;
 } else DB = process.env.DB_DEV;
 
 mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true,
-  })
+  .connect(
+    'mongodb+srv://jeanerix:K3X8OzJwmnwIXEs8@cluster0.yv9n2.mongodb.net/test?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => console.log('DB connected successfully!'));
 
 const port = process.env.PORT;
